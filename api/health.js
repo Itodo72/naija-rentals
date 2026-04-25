833cd0e83a752fb4fcdf050c33dd36ac9a089d4d
Itodo72 <abrahamitodo7@gmail.com>
2026-04-25 06:37:35 -0700

    Delete api directory

--- api/health.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(req, res) {
-  res.status(200).json({
-    status: 'healthy',
-    platform: 'NaijaRentals API',
-    version: '1.0.0',
-    timestamp: new Date().toISOString()
-  })
-}
